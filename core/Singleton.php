f6ed9667067b66ed4969e0ea7172451b310c0013
jonkofee <jonkofee@icloud.com>
2018-11-04 15:12:17 +0300

    :zap: singletone

---
+++ core/Singleton.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Core;
+
+trait Singleton
+{
+
+	protected static $instance;
+
+	final public static function getInstance()
+	{
+		return isset(static::$instance)
+			? static::$instance
+			: static::$instance = new static;
+	}
+
+}
